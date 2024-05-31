0638bbf7af3a2b7093f3996dc3d5976807bad4ab
aleperfect <143966062+aleperfect@users.noreply.github.com>
2024-05-31 17:11:05 -0500

    sdfds

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -4,17 +4,16 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `*🛍️https://chat.whatsapp.com/CCDGO1lIuH3Ggr4nA44cbY* ${pesan}`
-let teks = `*⬇️Venta de 🤖 ,regedits 🖥️📱 ,sensibilidades📱 ,+ 30 metodos ,cuentas ff y mass⬇️*
- ${oi}\n\n🛍️ *@Ale.izn_20:*\n`
+let oi = `*𝙼𝙴𝙽𝚂𝙰𝙹𝙴:* ${pesan}`
+let teks = `*⺀Invocando a las plantas 🪴⺀*\n\n🪐 ${oi}\n\n🪐 *𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂:*\n`
 for (let mem of participants) {
-teks += `⭐ @${mem.id.split('@')[0]}\n`}
-teks += `➤ 𝐑𝐞𝐚𝐝𝐳 𝐀𝐥𝐞𝐢𝐳𝐧`
+teks += `🌈 @${mem.id.split('@')[0]}\n`}
+teks += `└𝐊𝐈𝐓𝐓𝐘`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )
 }
 handler.help = ['tagall <mesaje>','invocar <mesaje>']
 handler.tags = ['group']
-handler.command = /^(tagall|invocar|invocacion|todos|invocación|aviso|despierten)$/i
+handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
 handler.admin = true
 handler.group = true
 export default handler
--- plugins/info-estado.js
+++ plugins/info-estado.js
@@ -28,9 +28,9 @@ const handler = async (m, { conn, usedPrefix }) => {
   const wm = 'The Mystic Bot';
   const info = ` ${tradutor.texto1[0]}
 
-  ${tradutor.texto1[1]} Aleizn
-  ${tradutor.texto1[2]} +51992621601
-  ${tradutor.texto1[3]} https://www.instagram.com/ale.izn_20/
+  ${tradutor.texto1[1]} 𝐊𝐈𝐓𝐓𝐘
+  ${tradutor.texto1[2]} 𝐊𝐈𝐓𝐓𝐘
+  ${tradutor.texto1[3]} 𝐊𝐈𝐓𝐓𝐘
 
   ${tradutor.texto1[4]} ${rtime}
   ${tradutor.texto1[5]} ${uptime}
